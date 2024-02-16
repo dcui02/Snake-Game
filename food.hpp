e0eb9c1866a936f106f30fd738824143467b2c46
David Cui <cuidavid02@gmail.com>
2024-02-16 14:41:58 -0500

    Create food.hpp

---
+++ food.hpp
@@ -0,0 +1,36 @@
+#ifndef FOOD_HPP
+#define FOOD_HPP
+
+#include <ncurses.h>
+
+//Two types of food
+////Either Increase or Decrease length of snake
+enum Type {Increase, Decrease}; 
+
+//Structure to hold properties of food
+struct Food {
+  int x;
+  int y;
+  char type;
+  struct Food* next;
+};
+
+typedef struct Food Food; 
+
+//Function prototypes 
+void add_new_food(Food* foods, Food* new_food);
+bool food_exists(Food* foods, int x, int y);
+Food* create_food(int x, int y, enum Type type);
+enum Type food_type(Food* foods, int x, int y);
+Food* remove_eaten_food(Food* foods, int x, int y);
+void draw_food(Food *food);
+
+#endif
+
+
+
+
+
+
+
+
